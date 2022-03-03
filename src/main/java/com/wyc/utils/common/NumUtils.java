5abadefaf4604c430a8d6e471e0234004a846fe2
MofFoam <2397521545@qq.com>
2022-03-03 14:19:54 +0800

    ..

--- src/main/java/com/wyc/utils/common/NumUtils.java
+++ src/main/java/com/wyc/utils/common/NumUtils.java
@@ -37,7 +37,6 @@ public class NumUtils {
      * @return
      */
     public static BigDecimal getBigDecimal(Object value) {
-        System.out.println();
         BigDecimal ret = null;
         if( value != null ) {
             if( value instanceof BigDecimal ) {
